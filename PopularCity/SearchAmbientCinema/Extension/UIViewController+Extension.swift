6417683a2b18ea9dd89e5e0973f4aca256cd860e
constdreamcoder <scdreamricher@gmail.com>
2024-01-24 21:39:53 +0900

    iOS 데뷔 과정 22회차 과제 - 주변 영화 탐색 과제에서 사용자 현재 위치 표시 기능 구현 완료

---
+++ PopularCity/SearchAmbientCinema/Extension/UIViewController+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewController+Extension.swift
+//  PopularCity
+//
+//  Created by SUCHAN CHANG on 1/24/24.
+//
+
+import UIKit
+
+extension UIViewController {
+//    func createActionSheet() {
+//        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+//        
+//        CinemaType.allCases.forEach { cinemaType in
+//            let cinemaButton = UIAlertAction(title: cinemaType.rawValue, style: .default) { [weak self] alertAction in
+//                guard let weakSelf = self else { return }
+//                weakSelf.searchCinemaLocation(with: alertAction.title!)
+//            }
+//            alert.addAction(cinemaButton)
+//        }
+//        
+//        let cancelButton = UIAlertAction(title: "취소", style: .cancel)
+//        
+//        alert.addAction(cancelButton)
+//        
+//        present(alert, animated: true)
+//    }
+}
--- PopularCity/SearchAmbientCinema/SearchAmbientCinemaViewController.swift
+++ PopularCity/SearchAmbientCinema/SearchAmbientCinemaViewController.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class SearchAmbientCinemaViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     let dataList = TheaterList.mapAnnotations
 
@@ -18,6 +21,9 @@ class SearchAmbientCinemaViewController: UIViewController {
         super.viewDidLoad()
                 
         configureNavigationBar()
+        searchCinemaLocation(with: CinemaType.showAll.rawValue)
+        configureCoreLocation()
+        checkDeviceLocationAuthorization()
     }
     
     @objc func filterBarButtonTapped() {
@@ -28,44 +34,89 @@ class SearchAmbientCinemaViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterBarButtonTapped))
     }
     
-    func createActionSheet() {
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        CinemaType.allCases.forEach { cinemaType in
-            let cinemaButton = UIAlertAction(title: cinemaType.rawValue, style: .default) { [weak self] alertAction in
-                guard let weakSelf = self else { return }
-                weakSelf.searchLocation(with: alertAction.title!)
+    func configureCoreLocation() {
+        locationManager.delegate = self
+    }
+}
+
+extension SearchAmbientCinemaViewController {
+    func checkDeviceLocationAuthorization() {
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                let authorization: CLAuthorizationStatus
+                
+                if #available(iOS 14, *) {
+                    authorization = self.locationManager.authorizationStatus
+                } else {
+                    authorization = CLLocationManager.authorizationStatus()
+                }
+                
+                DispatchQueue.main.async {
+                    self.checkCurrentLocationAuthorization(status: authorization)
+                }
             }
-            alert.addAction(cinemaButton)
         }
-        
-        let cancelButton = UIAlertAction(title: "취소", style: .cancel) { _ in
-            print("취소")
+    }
+    
+    func checkCurrentLocationAuthorization(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("notDetermined")
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            print("denied")
+            let coordinate = CLLocationCoordinate2D(latitude: 37.517742, longitude: 126.886463)
+            setRegionAndAnnotaionOnMapView(with: coordinate, annotationTitle: "청년사관학교 영등포 캠퍼스")
+            setLocationSettingAlert()
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            print("Error")
         }
+    }
+    
+    func setLocationSettingAlert() {
+        let alert = UIAlertController(title: "위치 정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기 '설정>개인정보 보호'에서 위치 서비스를 켜주세요", preferredStyle: .alert)
         
+        let goSettingButton = UIAlertAction(title: "설정으로 이동", style: .default) { _ in
+            if let settingUrl = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(settingUrl)
+            } else {
+                print("설정으로 이동하여 주세요")// 아마 토스토 이용해서 알림 ??
+            }
+        }
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel)
+    
+        alert.addAction(goSettingButton)
         alert.addAction(cancelButton)
         
-        present(alert, animated: true)
+        present(alert, animated: true, completion: nil)
     }
-    
-    func searchLocation(with type: String) {
-        mapView.removeAnnotations(mapView.annotations)
+}
+
+extension SearchAmbientCinemaViewController {
+    func searchCinemaLocation(with type: String) {
+        mapView.removeAnnotations(mapView.annotations.filter({ annotation in
+            return annotation.title != "내 위치"
+        }))
         
         dataList.forEach { theater in
+            let coordinate = CLLocationCoordinate2D(
+                latitude: theater.latitude,
+                longitude: theater.longitude
+            )
             if theater.type == type {
-                setRegionAndAnnotaionOnMapView(with: theater)
+                setRegionAndAnnotaionOnMapView(with: coordinate, annotationTitle: theater.location)
             } else if type == "전체보기" {
-                setRegionAndAnnotaionOnMapView(with: theater)
+                setRegionAndAnnotaionOnMapView(with: coordinate, annotationTitle: theater.location)
             }
         }
     }
     
-    func setRegionAndAnnotaionOnMapView(with theater: Theater) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: theater.latitude,
-            longitude: theater.longitude
-        )
-        
+    func setRegionAndAnnotaionOnMapView(with coordinate: CLLocationCoordinate2D, annotationTitle: String) {
+
         let region = MKCoordinateRegion(
             center: coordinate,
             latitudinalMeters: 20_000,
@@ -75,9 +126,57 @@ class SearchAmbientCinemaViewController: UIViewController {
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
-        annotation.title = theater.location
+        annotation.title = annotationTitle
         
         mapView.addAnnotation(annotation)
     }
-
+    
+    func createActionSheet() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        CinemaType.allCases.forEach { cinemaType in
+            let cinemaButton = UIAlertAction(title: cinemaType.rawValue, style: .default) { [weak self] alertAction in
+                guard let weakSelf = self else { return }
+                weakSelf.searchCinemaLocation(with: alertAction.title!)
+            }
+            alert.addAction(cinemaButton)
+        }
+        
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(cancelButton)
+        
+        present(alert, animated: true)
+    }
 }
+
+extension SearchAmbientCinemaViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            print(coordinate)
+            print(coordinate.latitude)
+            print(coordinate.longitude)
+            setRegionAndAnnotaionOnMapView(with: coordinate, annotationTitle: "내 위치")
+        }
+        
+        manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    // iOS 14이상
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkDeviceLocationAuthorization()
+    }
+    
+    // iOS 14 미만
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+
+    }
+}
+
+
+
